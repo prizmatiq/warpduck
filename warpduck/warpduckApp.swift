e4345e0620b21f11bd1336dd31ced1e23acead3d
Prizmatiq <prizmatiq@outlook.com>
2026-07-03 03:43:24 +0400

    Initial Commit

---
+++ warpduck/warpduckApp.swift
@@ -0,0 +1,17 @@
+//
+//  warpduckApp.swift
+//  warpduck
+//
+//  Created by Grinn on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct warpduckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
